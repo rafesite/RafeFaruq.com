058a38d8cff82d07efb49772424697edbfc5f0e7
rafesite <rafefaruq@gmail.com>
2026-06-24 22:08:22 -0500

    Added vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        'virtual-assistant': resolve(__dirname, 'virtual-assistant.html'),
+        'press-release': resolve(__dirname, 'press-release.html'),
+        'lead-generation': resolve(__dirname, 'lead-generation.html'),
+        'website-design': resolve(__dirname, 'website-design.html'),
+        'google-maps': resolve(__dirname, 'google-maps.html'),
+        privacy: resolve(__dirname, 'privacy.html'),
+        tos: resolve(__dirname, 'tos.html'),
+      },
+    },
+  },
+});
